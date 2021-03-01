79f38fdc032810853cd11dd86a904754b629a323
tbelz <tb00cs@gmail.com>
2021-03-01 13:14:26 +0100

    removed wrongly placed chars

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MS_CAPS, KC_Z  , KC_X  , KC_C  , KC_D  , KC_V  ,                         KC_K   , KC_H  ,KC_COMM, TD_DOT,KC_BSLS,KC_SLSH,
               KC_LBRC,KC_RBRC,  MS_CAPS,NAV_TAB, COMPOSE,                        KC_DOWN, KC_UP , SYM_ENT,   KC_LGUI, KC_GRV,
                                                  KC_SPC, OS_LSFT,            OS_RSFT,KC_BSPC
-  ),crrrsrsr
+  ),
   [_SYM] = LAYOUT_5x6_5(
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                         KC_F6  , KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11,
         KC_DOT , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
